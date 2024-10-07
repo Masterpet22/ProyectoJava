0944f195c49f70e120771668161c822359904438
Masterpet22 <eprenza@gmail.com>
2024-10-07 09:34:35 -0400

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/empresa/Login.java
@@ -0,0 +1,130 @@
+package com.mycompany.empresa;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author epren
+ */
+public class Login extends javax.swing.JFrame {
+
+    public Login() {
+        initComponents();
+        InitStyles();
+    }
+
+    private void InitStyles() {
+        btnIngresar.putClientProperty("JButton.buttonType", "roundRect");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        txNombre = new javax.swing.JTextField();
+        txPassword = new javax.swing.JPasswordField();
+        lbNombre = new javax.swing.JLabel();
+        lbPassword = new javax.swing.JLabel();
+        btnIngresar = new javax.swing.JButton();
+        txEstado = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("SGCA 3000");
+        setMinimumSize(new java.awt.Dimension(400, 600));
+        setName("login"); // NOI18N
+        setPreferredSize(new java.awt.Dimension(400, 600));
+        setSize(new java.awt.Dimension(400, 600));
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setMinimumSize(new java.awt.Dimension(400, 600));
+        bg.setPreferredSize(new java.awt.Dimension(400, 600));
+        bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/user.png"))); // NOI18N
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel1.setMaximumSize(new java.awt.Dimension(4, 512));
+        jLabel1.setMinimumSize(new java.awt.Dimension(400, 280));
+        jLabel1.setPreferredSize(new java.awt.Dimension(4, 512));
+        bg.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 380, 280));
+        bg.add(txNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 350, 320, 40));
+        bg.add(txPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 440, 320, 40));
+
+        lbNombre.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        lbNombre.setText("Usuario");
+        bg.add(lbNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 310, -1, -1));
+
+        lbPassword.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        lbPassword.setText("Contraseña");
+        bg.add(lbPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 400, -1, -1));
+
+        btnIngresar.setText("INGRESAR");
+        btnIngresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIngresarActionPerformed(evt);
+            }
+        });
+        bg.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 510, -1, -1));
+
+        txEstado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txEstado.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        bg.add(txEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 490, 400, 20));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(416, 608));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
+        String nombre = txNombre.getText();
+        String password = new String(txPassword.getPassword());
+
+        if (nombre.isEmpty() || password.isEmpty()) {
+            txEstado.setText("Error 01");
+        } else if (!nombre.equals("admin")) {
+            txEstado.setText("Error 01");
+        } else if (!password.equals("1234")) {
+            txEstado.setText("Error 01");
+        } else {
+            // Cerrar el JFrame actual
+            this.dispose();
+
+            // Abrir el nuevo JFrame
+            Principal frame2 = new Principal();
+            frame2.setVisible(true);
+        }
+    }//GEN-LAST:event_btnIngresarActionPerformed
+
+    public static void main(String args[]) {
+        FlatLightLaf.setup();
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
+    private javax.swing.JButton btnIngresar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lbNombre;
+    private javax.swing.JLabel lbPassword;
+    private javax.swing.JLabel txEstado;
+    private javax.swing.JTextField txNombre;
+    private javax.swing.JPasswordField txPassword;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/java/com/mycompany/empresa/Principal.java
@@ -0,0 +1,115 @@
+package com.mycompany.empresa;
+
+/**
+ *
+ * @author epren
+ */
+public class Principal extends javax.swing.JFrame {
+    public Principal() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnBackground = new javax.swing.JPanel();
+        pnLateral = new javax.swing.JPanel();
+        pnSuperior = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pnBackground.setBackground(new java.awt.Color(255, 255, 255));
+
+        pnLateral.setBackground(new java.awt.Color(204, 255, 255));
+
+        javax.swing.GroupLayout pnLateralLayout = new javax.swing.GroupLayout(pnLateral);
+        pnLateral.setLayout(pnLateralLayout);
+        pnLateralLayout.setHorizontalGroup(
+            pnLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 270, Short.MAX_VALUE)
+        );
+        pnLateralLayout.setVerticalGroup(
+            pnLateralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        pnSuperior.setBackground(new java.awt.Color(204, 255, 204));
+
+        javax.swing.GroupLayout pnSuperiorLayout = new javax.swing.GroupLayout(pnSuperior);
+        pnSuperior.setLayout(pnSuperiorLayout);
+        pnSuperiorLayout.setHorizontalGroup(
+            pnSuperiorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 810, Short.MAX_VALUE)
+        );
+        pnSuperiorLayout.setVerticalGroup(
+            pnSuperiorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 120, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout pnBackgroundLayout = new javax.swing.GroupLayout(pnBackground);
+        pnBackground.setLayout(pnBackgroundLayout);
+        pnBackgroundLayout.setHorizontalGroup(
+            pnBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnBackgroundLayout.createSequentialGroup()
+                .addComponent(pnLateral, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(pnSuperior, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnBackgroundLayout.setVerticalGroup(
+            pnBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnLateral, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnSuperior, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(1096, 728));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel pnBackground;
+    private javax.swing.JPanel pnLateral;
+    private javax.swing.JPanel pnSuperior;
+    // End of variables declaration//GEN-END:variables
+}
